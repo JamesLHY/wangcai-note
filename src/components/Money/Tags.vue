2aed74db3d53b163740cb1bdc88ada28117c950f
James <847798512@qq.com>
2020-04-18 17:29:02 +0800

    添加标签按钮完成

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="tags">
         <div class="new">
-            <button>新增标签</button>
+            <button @click="create">新增标签</button>
         </div>
         <ul class="current">
             <li v-for="tag in dataSource" :key="tag"
@@ -20,15 +20,24 @@
 
     @Component
     export default class Tags extends Vue {
-        @Prop() dataSource: string[] | undefined;
+        @Prop() readonly dataSource: string[] | undefined;
         selectedTags: string[] = [];
 
         toggle(tag: string) {
             const index = this.selectedTags.indexOf(tag);
-            if(index>=0){
-                this.selectedTags.splice(index,1);
-            }else{
+            if (index >= 0) {
+                this.selectedTags.splice(index, 1);
+            } else {
                 this.selectedTags.push(tag);
+            }
+        }
+
+        create() {
+            const name = window.prompt('请输入标签');
+            if (name === '') {
+                window.alert('标签不能为空');
+            } else if (this.dataSource) {
+                this.$emit('update:dataSource', [...this.dataSource, name]);
             }
         }
     }
@@ -47,16 +56,17 @@
             flex-wrap: wrap;
 
             > li {
-                $bg:#d9d9d9;
-                background:$bg ;
+                $bg: #d9d9d9;
+                background: $bg;
                 height: 24px;
                 line-height: 24px;
                 border-radius: (24px/2);
                 padding: 0 16px;
                 margin-right: 10px;
                 margin-top: 10px;
-                &.selected{
-                    background: darken($bg,50%);
+
+                &.selected {
+                    background: darken($bg, 50%);
                     color: white;
                 }
             }

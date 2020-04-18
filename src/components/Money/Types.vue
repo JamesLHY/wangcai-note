b345392693e57cc6e739f89b59d83638f0708e24
James <847798512@qq.com>
2020-04-18 13:46:43 +0800

    按照vue-class-component的文档使用props失败

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,42 +1,61 @@
 <template>
     <div>
         <ul class="types">
-            <li class="selected">支出</li>
-            <li>收入</li>
+            <li :class="type==='-' && 'selected'" @click="selectType('-')">支出</li>
+            <li :class="type === '+' && 'selected'"
+                @click="selectType('+')">收入
+            </li>
         </ul>
     </div>
 </template>
 
-<script>
-    export default {
-        name: "Types"
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+
+    @Component({
+        props: {
+            propMessage:String
+        }
+    })
+    export default class Types extends Vue {
+        type = '-';
+helloMsg = 'Hello,' + this.propMessage;
+        selectType(type: string) {
+            if (type !== '-' && type !== '+') {
+                throw new Error('type is unknown');
+            }
+            this.type = type;
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-    .types{
+    .types {
         background: #c4c4c4;
         display: flex;
         text-align: center;
         font-size: 24px;
-    > li{
-        width: 50px;
-        height: 64px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-    &.selected{
-    &::after{
-         content: '';
-         position: absolute;
-         bottom: 0;
-         left: 0;
-         width: 100%;
-         height: 4px;
-         background: #333333;
-     }
-    }
-    }
+
+        > li {
+            width: 50px;
+            height: 64px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+
+            &.selected {
+                &::after {
+                    content: '';
+                    position: absolute;
+                    bottom: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 4px;
+                    background: #333333;
+                }
+            }
+        }
     }
 </style>

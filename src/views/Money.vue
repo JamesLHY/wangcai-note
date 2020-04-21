d78b8fd68a0f31b661e3b2352d2dcaa879c6b8ab
James <847798512@qq.com>
2020-04-21 15:17:41 +0800

    将所有数据操作分装在store里

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,14 +18,15 @@
     import Types from '@/components/Money/Types.vue';
     import FormItem from '@/components/Money/FormItem.vue';
     import {Component} from 'vue-property-decorator';
+    import store from '@/store/index2';
     window.localStorage.setItem('version', '1.0.0');
 
     @Component({
         components: {FormItem, Types, Tags, NumberPad}
     })
     export default class Money extends Vue {
-        tags = window.tagList;
-        recordList= window.recordList;
+        tags = store.tagList;
+        recordList= store.recordList;
         record: RecordItem = {tags: [], notes: '', type: '-', amount: 0,};
 
         onUpdateTags(value: string[]) {
@@ -37,7 +38,7 @@
         }
 
         saveRecord() {
-window.createRecord(this.record)
+store.createRecord(this.record)
         }
 
 

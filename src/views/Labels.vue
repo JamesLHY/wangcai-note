50281198c9fc9496eec325cc052fa76c38d431c7
James <847798512@qq.com>
2020-04-23 15:33:31 +0800

    Labels.vue 使用vuex

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,8 @@
 <template>
     <Layout>
         <div class="tags">
-            <router-link class='tag' v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`"><span>{{tag.name}}</span>
+            <router-link class='tag' v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
+                <span>{{tag.name}}</span>
                 <Icon name="right"/>
             </router-link>
         </div>
@@ -12,21 +13,22 @@
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
     import {Component} from 'vue-property-decorator';
+    import Button from '@/components/Button.vue';
+    import { mixins } from 'vue-class-component';
+    import TagHelper from '@/mixins/TagHelper';
 
-    @Component
-    export default class Labels extends Vue {
-        //TODO
-        tags =[]; //store.tagList;
-
-        createTag() {
-            const name = window.prompt('请输入标签名');
-            if (name) {
-                // TODO
-                // store.createTag(name)
-
+    @Component({
+        components:{Button},
+        computed: {
+            tags() {
+                return this.$store.state.tagList;
             }
+        },
+    })
+    export default class Labels extends mixins(TagHelper) {
+        beforeCreate() {
+            this.$store.commit('fetchTags');
         }
     }
 </script>

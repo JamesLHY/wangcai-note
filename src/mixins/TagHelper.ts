fef23147c32f09a619b15bc17e4f59e05d5273e4
James <847798512@qq.com>
2020-04-25 16:47:08 +0800

    修复第一次进入页面弹出提示'创建成功'的bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,7 +1,9 @@
 
 import Vue from 'vue'
 import Component from 'vue-class-component'
-
+const map: {[key: string]: string} = {
+    'tag name duplicated': '标签名重复'
+};
 
 @Component
 export class TagHelper extends Vue {
@@ -11,6 +13,11 @@ export class TagHelper extends Vue {
             return window.alert('标签不能为空');
         }
         this.$store.commit('createTag', name);
+        if(this.$store.state.createTagError){
+            if(this.$store.state.createTagError.message){
+                window.alert(map[this.$store.state.createTagError.message] ||'未知错误')
+            }
+        }
     }
 }
 
